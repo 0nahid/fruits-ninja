ec95f681169960fbd1d962f6c002c67543476277
Nahid Hassan <nahidhassanbulbul@gmail.com>
2021-06-16 20:17:01 +0600

    25.50

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ let el = document.querySelectorAll('.thumb li');
 for (let i = 0; i < el.length; i++) {
     el[i].onclick = function () {
         var c = 0;
-        while (c < el.length){
+        while (c < el.length) {
             el[c++].className = 'check';
         }
         el[i].className = 'check active';
     }
+}
+// change background color 
+function changeBackground(color) {
+    const bg = document.querySelector('.bg');
+    bg.style.background = color;
+ }
+// change image on click
+function imgSlider(anything) {
+    document.querySelector('.fruits').src = anything;
 }
